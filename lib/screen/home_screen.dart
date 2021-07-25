797efb45f0f5dd466e8fe1976601d8e52a576750
Michael Mekuleyi <mekuleyimichael@gmail.com>
2021-07-25 03:23:39 +0100

    added new categories

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_app_ui/widgets/categories.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              "All Fruits",
+              style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Categories(),
+        ],
+      ),
+    );
+  }
+}
